7589193a237fae6604d262d72f16f76503271292
Austen Stone <austenstone@github.com>
2022-07-07 09:42:54 -0400

    logout

--- routes/api/logout.ts
+++ routes/api/logout.ts
@@ -4,7 +4,7 @@ export function handler(req: Request): Response {
   const headers = new Headers({
     "location": new URL(req.url).origin,
   });
-  deleteCookie(headers, "deploy_access_token");
+  deleteCookie(headers, "gh_access_token");
   return new Response(null, {
     status: 302,
     headers,
